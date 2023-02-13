9953119704176e4c7587a79c496072e980f701a5
dampion <evanchen79@gmail.com>
2023-02-13 11:38:00 +0800

    Fix# vite config

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,18 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  lib: {
-    entry: path.resolve(__dirname, 'src/comoponents/index.js'),
-    name: 'vue3-damp-fireworks',
-    fileName: (format) => `vue3-damp-fireworks`
-  },
-  rollupOptions: {
-    external: ['vue'],
-    output: {
-      globals: {
-        vue: 'Vue'
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/components/index.js'),
+      name: 'vue3-damp-fireworks',
+      fileName: (format) => `vue3-damp-fireworks.${format}.js`
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
       }
     }
   }
